91c38b63961186780636d2641efb70d687c7951b
Constantin Bessonov <beskos@mail.ru>
2025-08-07 21:34:50 +0300

    demoqa_test

--- src/test/java/tests/PracticeFormTests.java
+++ src/test/java/tests/PracticeFormTests.java
@@ -1,76 +1,121 @@
 package tests;
 
+import com.codeborne.selenide.logevents.SelenideLogger;
+import helpers.Attach;
+import io.qameta.allure.selenide.AllureSelenide;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.Test;
 import pages.RegistrationPage;
 
+import static io.qameta.allure.Allure.step;
+
 import static data.TestData.*;
 
+@Tag("demoqa")
 public class PracticeFormTests extends TestBase {
 
     RegistrationPage registrationPage = new RegistrationPage();
 
 
+    @AfterEach
+    void addAttachments() {
+        Attach.screenshotAs("Last screenshot");
+        Attach.pageSource();
+        Attach.browserConsoleLogs();
+        Attach.addVideo();
+
+    }
+
     @Test
     void successfulRegistrationTest() {
+        SelenideLogger.addListener("allure", new AllureSelenide());
 
-        registrationPage.openPage()
-                .removeBanner()
-                .setFirstName(firstName)
-                .setLastName(lastName)
-                .setEmail(userEmail)
-                .setGender(gender)
-                .setUserNumber(mobilePhone)
-                .setDateOfBirth(date, month, year)
-                .setSubjects(subjects)
-                .setHobbies(hobbies)
-                .setPicture(picture)
-                .setCurrentAddress(streetAddress)
-                .setState(state)
-                .setCity(city)
-                .submitPracticeForm();
+        step("Открываем сайт с формой", () -> {
+            registrationPage.openPage();
+        });
+        step("Заполняем форму всеми данными", () -> {
+            registrationPage.removeBanner()
+                    .setFirstName(firstName)
+                    .setLastName(lastName)
+                    .setEmail(userEmail)
+                    .setGender(gender)
+                    .setUserNumber(mobilePhone)
+                    .setDateOfBirth(date, month, year)
+                    .setSubjects(subjects)
+                    .setHobbies(hobbies)
+                    .setPicture(picture)
+                    .setCurrentAddress(streetAddress)
+                    .setState(state)
+                    .setCity(city);
+        });
 
-        registrationPage.verifyResultsAppears()
-                .checkResult("Student Name", firstName + " " + lastName)
-                .checkResult("Student Email", userEmail)
-                .checkResult("Gender", gender)
-                .checkResult("Mobile", mobilePhone)
-                .checkResult("Date of Birth", date + " " + month + "," + year)
-                .checkResult("Subjects", subjects)
-                .checkResult("Hobbies", hobbies)
-                .checkResult("Picture", picture)
-                .checkResult("Address", streetAddress)
-                .checkResult("State and City", state + " " + city);
+        step("Отравляем заполненную форму", () -> {
+            registrationPage.submitPracticeForm();
+        });
 
+        step("Проверка результатов заполнения", () -> {
+            registrationPage.verifyResultsAppears()
+                    .checkResult("Student Name", firstName + " " + lastName)
+                    .checkResult("Student Email", userEmail)
+                    .checkResult("Gender", gender)
+                    .checkResult("Mobile", mobilePhone)
+                    .checkResult("Date of Birth", date + " " + month + "," + year)
+                    .checkResult("Subjects", subjects)
+                    .checkResult("Hobbies", hobbies)
+                    .checkResult("Picture", picture)
+                    .checkResult("Address", streetAddress)
+                    .checkResult("State and City", state + " " + city);
+        });
     }
 
     @Test
     void successMinFormTest() {
 
-        registrationPage.openPage()
-                .setFirstName(firstName)
-                .setLastName(lastName)
-                .setGender(gender)
-                .setUserNumber(mobilePhone)
-                .submitPracticeForm();
+        step("Открываем сайт с формой", () -> {
+            registrationPage.openPage();
+        });
 
-        registrationPage.verifyResultsAppears()
-                .checkResult("Student Name", firstName + " " + lastName)
-                .checkResult("Gender", gender)
-                .checkResult("Mobile", mobilePhone);
+        step("Заполняем форму обязательными полями", () -> {
+            registrationPage.setFirstName(firstName)
+                    .setLastName(lastName)
+                    .setGender(gender)
+                    .setUserNumber(mobilePhone);
+        });
+
+        step("Отравляем заполненную форму", () -> {
+            registrationPage.submitPracticeForm();
+        });
+
+        step("Проверка результаты заполнения обязательных полей", () -> {
+            registrationPage.verifyResultsAppears()
+                    .checkResult("Student Name", firstName + " " + lastName)
+                    .checkResult("Gender", gender)
+                    .checkResult("Mobile", mobilePhone);
+        });
     }
 
     @Test
     void negativeMinFormTest() {
 
-        registrationPage.openPage()
-                .setFirstName(firstName)
-                .setLastName(lastName)
-                .setGender(gender)
-                .setUserNumber("")
-                .submitPracticeForm();
+        step("Открываем сайт с формой", () -> {
+            registrationPage.openPage();
+        });
 
-        registrationPage.verifyNegativeResultsAppears();
+        step("Заполняем форму обязательными полями", () -> {
+            registrationPage.setFirstName(firstName)
+                    .setLastName(lastName)
+                    .setGender(gender)
+                    .setUserNumber("");
+        });
+
+        step("Отравляем заполненную форму", () -> {
+            registrationPage.submitPracticeForm();
+        });
+
+        step("Проверка что появилась ошибка", () -> {
+            registrationPage.verifyNegativeResultsAppears();
+        });
     }
-
 
 }
